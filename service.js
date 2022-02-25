777aff6a6bfdf4cb43cdac628e06594ca8a0e032
ggozlo <ggozlo@naver.com>
2022-02-25 14:21:09 +0900

    서비스 수정

--- service.js
+++ service.js
@@ -4,21 +4,7 @@ exports.Service =  class Service {
         this.dao = dao
     }
 
-    // save 이벤트
-    save(task) {
-
-        return new Promise((resolve, reject) => {
-            const insertResult = this.dao.execute(
-                `INSERT INTO book_list
-                (book_name, book_author, book_ISBN, book_publisher, publication_date ) 
-                values ('${task.command[0]}','${task.command[1]}','${task.command[2]}'
-                ,'${task.command[3]}','${task.command[4]}');` , 
-                task);
-            resolve(insertResult);
-        });
-    };
-
-    // show 이벤트
+    // show 메서드
     show(task) {
         const secondCommand = task.shiftCommand();
         const selectQuery = `SELECT 
@@ -40,50 +26,6 @@ exports.Service =  class Service {
         })
     };
 
-    // modify 이벤트
-    modify(task) { 
-
-        return new Promise((resolve, reject) => {
-        
-            targetCheck(this.dao,task)
-            .then( (task) => {
-                this.dao.execute(`
-                    UPDATE book_list SET 
-                        book_name = '${task.command[1]}',
-                        book_author = '${task.command[2]}',
-                        book_ISBN = '${task.command[3]}',
-                        book_publisher = '${task.command[4]}', 
-                        publication_date = '${task.command[5]}'
-                        WHERE book_id = ${task.command[0]};` ,
-                        task)
-                    .then((task) => {
-                        // 여기서 데이터 가공... 
-                        resolve(task);
-                    });
-                
-            });
-        });
-    };
-
-    // remove 메소드
-    remove(task) {
-        const secondCommand = task.shiftCommand();
-        return new Promise((resolve, reject) => {
-            
-            if( secondCommand == 'all' ) {
-                resolve(this.dao.execute(`truncate booK_list;` ,task ));
-            } else if (secondCommand == 'id') {
-                targetCheck(this.dao, task)
-                .then( (task) => {
-                    resolve(this.dao.execute(`
-                        DELETE FROM book_list
-                            WHERE book_id = ${task.command[0]} ;` ,task ));
-                });
-            } else {
-                reject(new Error('잘못된 서브 커맨드 입니다.'));
-            }
-        });
-    };
 }
 // 공통함수
 function targetCheck(dao, task)  {
